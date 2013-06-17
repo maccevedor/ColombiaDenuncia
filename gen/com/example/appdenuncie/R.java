fadde22ed8780f384112e8bc6134796a4c93c128
root <root@buc21>
2013-06-17 14:58:11 -0500

    Funcionando List con base de datos en Sql Lite

--- gen/com/example/appdenuncie/R.java
+++ gen/com/example/appdenuncie/R.java
@@ -24,7 +24,12 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f080005;
+        public static final int btn_add=0x7f080002;
+        public static final int input_label=0x7f080001;
+        public static final int spinner=0x7f080004;
+        public static final int textView1=0x7f080000;
+        public static final int textView2=0x7f080003;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -36,6 +41,7 @@ public final class R {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050002;
+        public static final int spinner_title=0x7f050003;
     }
     public static final class style {
         /** 
