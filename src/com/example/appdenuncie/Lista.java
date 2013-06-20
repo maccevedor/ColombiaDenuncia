fe025b544605190d83a098981bec34891bd0e9d0
Mauricio Acevedo <maccevedor@gmail.com>
2013-06-20 16:17:22 -0500

    Completa la Lista , y arreglos visuales al formulario.

--- src/com/example/appdenuncie/Lista.java
+++ src/com/example/appdenuncie/Lista.java
@@ -3,7 +3,9 @@ package com.example.appdenuncie;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
-import android.widget.Spinner;
+import android.widget.Spinner;	
+import android.widget.ArrayAdapter;
+
 
 public class Lista extends Activity {
 
@@ -11,7 +13,32 @@ public class Lista extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_lista);
+		
+		// Initialize the array
+	    String[] Empresas = { "Emab", "Proactiva", "AMB" };
+	 
+	    // Declare the UI components
+	     Spinner monthsListView;
+	 
+	    ArrayAdapter arrayAdapter;
 	
+
+	        setContentView(R.layout.activity_lista);
+	 
+	        // Initialize the UI components
+	        monthsListView = (Spinner) findViewById(R.id.spinner);
+	        // For this moment, you have ListView where you can display a list.
+	        // But how can we put this data set to the list?
+	        // This is where you need an Adapter
+	 
+	        // context - The current context.
+	        // resource - The resource ID for a layout file containing a layout 
+	                // to use when instantiating views.
+	        // From the third parameter, you plugged the data set to adapter
+	        arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, Empresas);
+	 
+	        // By using setAdapter method, you plugged the ListView with adapter
+	        monthsListView.setAdapter(arrayAdapter);
 		
 	}
 
